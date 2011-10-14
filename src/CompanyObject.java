7621bf9a9ea57d53facc09bc41cbf885ef611843
Brian Sweeney <brians200@gmail.com>
2011-10-14 15:11:29 -0500

    Added Company Object toString

--- src/CompanyObject.java
+++ src/CompanyObject.java
@@ -24,5 +24,19 @@ public class CompanyObject {
 		summaryQuote = a[8];
 		exchange = a[9];
 	}
+	
+	public String toString()
+	{
+		return "Symbol: "+ symbol+
+				"Name: " +name+
+				"Last Sale: " + lastSale +
+				"Market Cap: " + marketCap +
+				"ADR TSO: " + adrTSO +
+				"IPO Year: "+ipoYear+
+				"Sector: " +sector+
+				"Industry: " + industry+
+				"Summary Quote: "+ summaryQuote +
+				"Exchange: "+exchange;
+	}
 
 }
