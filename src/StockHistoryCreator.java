7e99a2ec378673bacea95341c5433827035ec79c
unknown <fireline@fireline-PC.(none)>
2011-10-14 16:47:25 -0700

    Added prepared statements

--- src/StockHistoryCreator.java
+++ src/StockHistoryCreator.java
@@ -1,24 +1,49 @@
 import java.util.ArrayList;
-
+import java.sql.*;
 
 public class StockHistoryCreator {
 
 	/**
 	 * @param args
 	 */
+	private static Connection connect = null;
+	private static PreparedStatement preparedStatement = null;
+	
 	public static void main(String[] args) {
-		ArrayList<CompanyObject> companies = CompanyDownloader.DownloadCompanyTables();
-		for(CompanyObject company:companies)
-		{
-			ArrayList<StockObject> stockHistory = StockDownloader.DownloadEntireHistory(company.symbol);
-			//INSERT COMPANY
-			for(StockObject stockObject:stockHistory)
-			{
-				System.out.println(stockObject);
-				System.out.println(company);
-				//INSERT HISTORY
+		
+		
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		}
+		
+			ArrayList<CompanyObject> companies = CompanyDownloader.DownloadCompanyTables();
+			for(CompanyObject company:companies)
+			{
+				ArrayList<StockObject> stockHistory = StockDownloader.DownloadEntireHistory(company.symbol);
+				//INSERT COMPANY
+				try {
+					preparedStatement = connect.prepareStatement("INSERT into TABLE Stock VALUES("+company.exchange+","+company.symbol+","+company.name+","+company.ipoYear+","+company.industry+","+company.marketCap+","+company.sector+")");
+					preparedStatement.executeUpdate();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				for(StockObject stockObject:stockHistory)
+				{
+					System.out.println(stockObject);
+					System.out.println(company);
+					try {
+						preparedStatement = connect.prepareStatement("INSERT into TABLE Stock VALUES("+company.exchange+","+stockObject.symbol+","+stockObject.date+","+stockObject.high+","+stockObject.low+","+stockObject.close+","+stockObject.volume+","+stockObject.adjClose+")");
+						preparedStatement.executeUpdate();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 	}
 
 }
