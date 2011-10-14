bed3420f2e9fd7a16ac16716977631979808a8bf
Brian Sweeney <brians200@gmail.com>
2011-10-14 17:12:27 -0500

    Working

--- src/StockHistoryCreator.java
+++ src/StockHistoryCreator.java
@@ -14,18 +14,23 @@ public class StockHistoryCreator {
 		
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
+				connect = DriverManager.getConnection("jdbc:mysql://mysql.cis.ksu.edu/bsweeney","bsweeney", "a1b2c3d4e5");
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		
+			
 			ArrayList<CompanyObject> companies = CompanyDownloader.DownloadCompanyTables();
 			for(CompanyObject company:companies)
 			{
 				ArrayList<StockObject> stockHistory = StockDownloader.DownloadEntireHistory(company.symbol);
 				//INSERT COMPANY
+				String temp = "INSERT into Stock VALUES('"+company.exchange+"','"+company.symbol+"','"+company.name+"',"+company.ipoYear+",'"+company.industry+"',"+company.marketCap+",'"+company.sector+"')";
 				try {
-					preparedStatement = connect.prepareStatement("INSERT into TABLE Stock VALUES("+company.exchange+","+company.symbol+","+company.name+","+company.ipoYear+","+company.industry+","+company.marketCap+","+company.sector+")");
+					preparedStatement = connect.prepareStatement(temp);
 					preparedStatement.executeUpdate();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
@@ -33,10 +38,11 @@ public class StockHistoryCreator {
 				}
 				for(StockObject stockObject:stockHistory)
 				{
-					System.out.println(stockObject);
-					System.out.println(company);
+					//System.out.println(stockObject);
+					//System.out.println(company);
 					try {
-						preparedStatement = connect.prepareStatement("INSERT into TABLE Stock VALUES("+company.exchange+","+stockObject.symbol+","+stockObject.date+","+stockObject.high+","+stockObject.low+","+stockObject.close+","+stockObject.volume+","+stockObject.adjClose+")");
+						String temp2 = "INSERT into History VALUES('"+company.exchange+"','"+stockObject.symbol+"','"+stockObject.date+"',"+stockObject.high+","+stockObject.low+","+stockObject.close+","+stockObject.volume+","+stockObject.adjClose+")";
+						preparedStatement = connect.prepareStatement(temp2);
 						preparedStatement.executeUpdate();
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
